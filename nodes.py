92d5503aeb2e1bfb8374b845cf7a75bc6bce9291
Andre Mojica <aisumohee@gmail.com>
2025-12-03 21:04:03 -0600

    Added example nodes
    created example nodes to test upcoming algorithm

--- nodes.py
+++ nodes.py
@@ -1,8 +1,8 @@
-class Nodes:
-    def __init__(self, name, depart, code, attrib, prereqs):
+class Node:
+    def __init__(self, name, id, depart, attrib, prereqs):
         self.__name = name
+        self.__id = id
         self.__depart = depart
-        self.__code = code
         self.__attrib = attrib
         self.__prereqs = prereqs
         # maybe add info variable but not part of MVP
@@ -13,8 +13,8 @@ class Nodes:
     def getDepart(self):
         return self.__depart
 
-    def getCode(self):
-        return self.__code
+    def getID(self):
+        return self.__id
 
     def getAttrib(self):
         return self.__attrib
@@ -22,5 +22,46 @@ class Nodes:
     def getPrereqs(self):
         return self.__prereqs
 
+# Example Nodes
+COMP123 = Node(
+    "Core Concepts in Computer Science",
+    "COMP 123",
+    "CS",
+    {"100 Level", "Required"},
+    []
+)
 
+COMP127 = Node(
+    "Object-Oriented Programming and Abstraction",
+    "COMP 127",
+    "CS",
+    ["100 Level", "Required"],
+    [{"COMP 123"}]
+)
+
+COMP128 = Node(
+    "Data Structures",
+    "COMP 128",
+    "CS",
+    ["100 Level", "Required"],
+    [{"COMP 127"}]
+)
+
+COMP221 = Node(
+    "Algorithm Design and Analysis",
+    "COMP 221",
+    "CS",
+    ["200 Level", "Required"],
+    [{"COMP 128"}, {"MATH 279"}]
+)
+# prereq structure
+# prereqs = [
+#   {pq1,pq2},  the stuff inside the curly brackets is OR
+#   {pq3,pq4},  the stuff curly brackets themselves is AND
+# ] this means prereqs are (pq1 or pq2) AND (pq3 or pq4)
+
+if __name__ == "__main__":
+    print(COMP123.getName())
+    print(COMP123.getDepart())
+    print(COMP123.getID())
 
