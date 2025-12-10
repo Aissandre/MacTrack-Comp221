df793cd24fa19aa7558351df9bd7455d232f4d74
Bryant Juarez <156694695+BryantBB@users.noreply.github.com>
2025-12-09 19:14:15 -0600

    Update nodes.py
    Reverted it

--- nodes.py
+++ nodes.py
@@ -1,19 +1,11 @@
 class Node:
-
-    def __init__(self, name, id, depart, exPreReqs, opPreReqs):
-        """
-        Initializes a node that represents an MSCS course.
-        :param name: Course title
-        :param id: Course number
-        :param depart: Department name
-        :param exPreReqs: Exclusive pre-requisites
-        :param opPreReqs: Optional pre-requisites
-        """
+    def __init__(self, name, id, depart, attrib, prereqs):
         self.__name = name
         self.__id = id
         self.__depart = depart
-        self.__exPreReqs = exPreReqs
-        self.__opPreReqs = opPreReqs
+        self.__attrib = attrib
+        self.__prereqs = prereqs
+        # maybe add info variable but not part of MVP
 
     def getName(self):
         return self.__name
@@ -24,8 +16,14 @@ class Node:
     def getID(self):
         return self.__id
 
-    def getExPreReqs(self):
-        return self.__exPreReqs
-    
-    def getOpPreReqs(self):
-        return self.__opPreReqs
+    def getAttrib(self):
+        return self.__attrib
+
+    def getPrereqs(self):
+        return self.__prereqs
+
+# prereq structure
+# prereqs = [
+#   {pq1,pq2},  the stuff inside the curly brackets is OR
+#   {pq3,pq4},  the stuff curly brackets themselves is AND
+# ] this means prereqs are (pq1 or pq2) AND (pq3 or pq4)
